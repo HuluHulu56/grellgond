4607efd6e2895d31d48d0c0ed9d95b7ac03c2245
HuluHulu56 <80635912+HuluHulu56@users.noreply.github.com>
2026-04-23 18:58:27 +0200

    Add setup.py for Grellgond library

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='grellgond',
+    version='0.1.0',
+    description='Official library for ℷ (The Grellgond mathematical constant)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Simon & Villiam',
+    url='https://github.com/HuluHulu56/grellgond',
+    packages=find_packages(),
+    install_requires=['sympy'],
+    entry_points={
+        'console_scripts': [
+            'grellgond=grellgond.cli:main',
+        ],
+    },
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
